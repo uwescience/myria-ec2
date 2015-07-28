16171a1a4770b38948737192a37203c55c94dac6
jortiz16 <jortiz16@uw.edu>
2015-07-27 21:18:32 -0700

    mount existing ebs volumes

--- postgresplugin.py
+++ postgresplugin.py
@@ -9,20 +9,40 @@ DEFAULT_DATA_PATH = '/mnt/postgresdata'
 DEFAULT_PATH_FORMAT = '/usr/lib/postgresql/{version}/bin'
 DEFAULT_PATH = DEFAULT_PATH_FORMAT.format(version=DEFAULT_VERSION)
 
+MOUNT_ENABLED = False
+
 class PostgresInstaller(DefaultClusterSetup):
     def __init__(self,
                  port=DEFAULT_PORT,
                  version=DEFAULT_VERSION,
                  database_path=DEFAULT_DATA_PATH,
                  install_on_master=True,
-                 new_ebs=False):
+                 new_ebs_enable=False,
+                 new_ebs_type=None,
+                 new_ebs_region=None,
+                 new_ebs_availability_zone=None,
+                 new_ebs_size=None,
+                 mount_existing_ebs_enable=False,
+                 mount_existing_ebs_list=None,
+                 mount_existing_ebs_region=None):
         super(PostgresInstaller, self).__init__()
 
         self.port = port
         self.version = version
-        self.database_path = database_path
         self.install_on_master = install_on_master
-        self.new_ebs = new_ebs
+
+        #ebs storage options
+        self.new_ebs = {'enabled': True if new_ebs_enable=="TRUE" else False,
+                        'size': new_ebs_size, 'type': new_ebs_type,
+                        'region':new_ebs_region, 'availability_zone': new_ebs_availability_zone}
+
+        global MOUNT_ENABLED
+        MOUNT_ENABLED = True if mount_existing_ebs_enable=="TRUE" else False
+        self.mount_existing_ebs = {'enabled': MOUNT_ENABLED,
+                                    'list_volumes': mount_existing_ebs_list,
+                                    'region': mount_existing_ebs_region}
+
+        self.database_path = database_path if not (self.mount_existing_ebs['enabled'] or self.new_ebs['enabled']) else '/data_mount'
 
         # Generated properties
         self.log = "{}/server.log".format(database_path)
@@ -36,27 +56,46 @@ class PostgresInstaller(DefaultClusterSetup):
         if not node.is_master() or self.install_on_master:
             log.info("Setting up postgres on {}".format(node.alias))
 
-            if self.new_ebs is True:
-                self.database_path = '/data_mount'
+            if self.new_ebs['enabled'] and not node.is_master():
+                log.info("Enabling new EBS")
                 node.ssh.execute('mkdir /data_mount')
 
                 log.info('create new EBS volume for ' + str(node.alias))
                 
-                create_drive_command = "aws ec2 create-volume --size 10 --region us-east-1 --availability-zone us-east-1b --volume-type gp2"
+                create_drive_command = "aws ec2 create-volume --size {} --region {} --availability-zone {} --volume-type {}".format(self.new_ebs['size'], self.new_ebs['region'], self.new_ebs['availability_zone'], self.new_ebs['type'])
+
                 output_create_drive = os.popen(create_drive_command).read()
                 volume_name = output_create_drive.split('\t')[7]
 
                 time.sleep(10)
 
                 log.info('attaching an EBS volume to ' + str(node.alias))
-                attach_volume_command = "aws ec2 attach-volume  --region us-east-1 --volume-id " + volume_name + " --instance-id " + node.id + " --device /dev/sdc"
+
+                attach_volume_command = "aws ec2 attach-volume --region {} --volume-id {} --instance-id {} --device /dev/sdc".format(self.new_ebs['region'], volume_name, node.id)
                 os.system(attach_volume_command)
 
                 time.sleep(20)
 
                 #mounting new drive
-                node.ssh.execute('lsblk')
                 node.ssh.execute('sudo mkfs -t ext4 /dev/xvdc;')
+                node.ssh.execute('sudo mount /dev/xvdc /data_mount;')
+
+            if self.mount_existing_ebs['enabled'] and not node.is_master():
+                log.info("Mounting existing EBS drives")
+                node.ssh.execute('mkdir /data_mount')
+
+                listOfVolumes = self.mount_existing_ebs['list_volumes'].split(',')
+                log.info('attaching an EBS volume to ' + str(node.alias))
+
+                workerNum = int(node.alias[-2:])
+
+                attach_volume_command = "aws ec2 attach-volume --region {} --volume-id {} --instance-id {} --device /dev/sdc".format(self.mount_existing_ebs['region'],listOfVolumes[workerNum-1], node.id)
+                log.info(attach_volume_command)
+
+                os.system(attach_volume_command)
+
+                time.sleep(20)
+
                 node.ssh.execute('sudo mount /dev/xvdc /data_mount;')
 
             node.ssh.execute('sudo add-apt-repository -r "deb http://www.cs.wisc.edu/condor/debian/development lenny contrib"')
@@ -71,6 +110,8 @@ class PostgresInstaller(DefaultClusterSetup):
                 """.format(
                            pg_path=self.path, data=self.database_path,
                            opt=self.options, log=self.log)
+            #extra
+            log.info(start_pg)
             node.ssh.execute(start_pg)
 
             # sleep 5 seconds wait for postgres start
@@ -91,11 +132,16 @@ class PostgresInstaller(DefaultClusterSetup):
 
     @staticmethod
     def create_user(node, user, password, path=DEFAULT_PATH, port=DEFAULT_PORT):
-        sql = "CREATE USER {user} WITH PASSWORD \'{password}\'".format(user=user, password=password)
-        conditional_command = """sudo -u postgres psql -tAc "SELECT 1 FROM pg_roles WHERE rolname='{user}'" | grep -q 1""".format(user=user)
-        create_command = """sudo -u postgres {path}/psql -p {port} -c "{sql}" """.format(path=path, port=port, sql=sql)
-        command = conditional_command + '||' + create_command
-        return PostgresInstaller._execute(node, command, path)
+        if MOUNT_ENABLED and not node.is_master() :
+            sql = "ALTER USER {user} WITH PASSWORD \'{password}\'".format(user=user, password=password)
+            create_command = """sudo -u postgres {path}/psql -p {port} -c "{sql}" """.format(path=path, port=port, sql=sql)
+            return PostgresInstaller._execute(node, create_command, path)
+        else:
+            sql = "CREATE USER {user} WITH PASSWORD \'{password}\'".format(user=user, password=password)
+            conditional_command = """sudo -u postgres psql -tAc "SELECT 1 FROM pg_roles WHERE rolname='{user}'" | grep -q 1""".format(user=user)
+            create_command = """sudo -u postgres {path}/psql -p {port} -c "{sql}" """.format(path=path, port=port, sql=sql)
+            command = conditional_command + '||' + create_command
+            return PostgresInstaller._execute(node, command, path)
 
     @staticmethod
     def grant_all(node, name, user, path=DEFAULT_PATH, port=DEFAULT_PORT):
@@ -109,7 +155,8 @@ class PostgresInstaller(DefaultClusterSetup):
         conditional_command = """sudo -u postgres psql -tAc "SELECT 1 FROM pg_database WHERE datname = '{name}'" | grep -q 1""".format(name=name)
         create_command = """sudo -u postgres {pg_path}/psql -p {port} -c "CREATE DATABASE {db}" """.format(pg_path=path, port=port, db=name)
         command = conditional_command + '||' + create_command
-        return PostgresInstaller._execute(node, command, path)
+        if not MOUNT_ENABLED or node.is_master():
+            PostgresInstaller._execute(node, command, path)
 
     @staticmethod
     def set_listeners(node, listeners, path='/etc/postgresql/{version}/main/postgresql.conf', version=DEFAULT_VERSION):
@@ -135,10 +182,11 @@ class PostgresInstaller(DefaultClusterSetup):
                             restart=True):
         PostgresInstaller.stop(node)
 
-        node.ssh.execute("sudo mkdir -m 700 -p {}".format(data_path))
-        node.ssh.execute(r"""sudo cp -rp `grep -Po "data_directory\\s*=\\s*'\K[^']*(?=')" {config_path}`/* {data_path}""".format(
-            config_path=config_path.format(version=version),
-            data_path=data_path))
+        if not MOUNT_ENABLED or node.is_master():
+            node.ssh.execute("sudo mkdir -m 700 -p {}".format(data_path))
+            node.ssh.execute(r"""sudo cp -rp `grep -Po "data_directory\\s*=\\s*'\K[^']*(?=')" {config_path}`/* {data_path}""".format(
+                config_path=config_path.format(version=version),
+                data_path=data_path))
 
         # Just in case directory already existed
         node.ssh.execute("sudo chmod 700 {}".format(data_path))
@@ -170,10 +218,12 @@ class PostgresInstaller(DefaultClusterSetup):
 
     @staticmethod
     def initialize_database(node, database_path, path=DEFAULT_PATH):
-        node.ssh.execute("sudo -u postgres {pg_path}/initdb -D {data_path}".format(
-            pg_path=path, data_path=database_path))
+        command = "sudo -u postgres {pg_path}/initdb -D {data_path}".format(pg_path=path, data_path=database_path)
+        if not MOUNT_ENABLED or node.is_master():
+            node.ssh.execute(command)
 
     @staticmethod
     def _execute(node, command, path=DEFAULT_PATH):
         cd = "cd {}".format(path)
+        log.info(';'.join([cd, command]))
         return node.ssh.execute(';'.join([cd, command]))
